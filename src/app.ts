40048d19a94b4bd9ee926196f6ac58e5446d9e6f
Giselle Georges <giselle@nextintl.com>
2026-07-09 12:04:07 -0400

    deploy on railway

--- src/app.ts
+++ src/app.ts
@@ -15,8 +15,8 @@ const app = express();
 app.use(
   cors({
     origin:[
-       "http://localhost:3000",
-    "http://localhost:3001",
+       "restaurant-feedback-dashboard.vercel.app",
+       "restaurant-feedback-frontend-lovat.vercel.app",
       ],
     credentials: true,
   })
